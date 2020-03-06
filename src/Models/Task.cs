ef9c80900c0ea4aae7392f329f3e18a9a092207a
Goncharik Yury <ygoncharik@qulix.com>
2020-03-06 18:28:38 +0300

    Implemented models: Employee, Project, Task. Implemented connection to DB, writing employee to DB and reading list of employees from DB. Also implemented some views and utility class.

---
+++ src/Models/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Training_task.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int TimeToComplete { get; set; } // ??? Формат
+        public DateTime BeginDate { get; set; } // ??? В каком формате даты?
+        public DateTime EndDate { get; set; } // ??? Формат
+        public TaskStatus Status { get; set; } // ??? Какой тип должен быть? Можно enum?
+    }
+}
